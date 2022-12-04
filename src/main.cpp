5445b7bb300ee54f048a7f59c6afab2799036c9f
Pneuma <jacobcorte8@gmail.com>
2022-12-03 22:15:00 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+  #include <aws/core/Aws.h>
+  #include <lua.hpp>
+
+  #include <Framework.h>
+
+  int main(int numArgs, char* args[]) {
+          lua_State* lua;
+          // create a web socket connection
+
+
+          // Create a web socket connection
+
+          return 0;
+  }
